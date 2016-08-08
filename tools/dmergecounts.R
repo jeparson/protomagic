3150f259a82b3ad51ea771a53527d6888623523b
jeparson <jerod.parsons@nist.gov>
2016-08-08 13:08:50 -0700

    Define & test MergeCounts App. Input: Source_Files: List of quantification output Column: Which column of output contains counts names: Identifiers for each source_file outputfilename:  Where to write the results.
    Output:  Outputfilename.csv [comma-separated]

--- tools/dmergecounts.R
+++ tools/dmergecounts.R
@@ -2,20 +2,20 @@ library(sevenbridges)
 library(readr)
 #reads local file, but defines it on the job / image
 # Any changes to the inputs or positions must be doubled in both definedashboard.R and runDashboard.R
-script=fileDef(name='merge_count_data.R',content=read_file('merge_count_data.R'))
+script=fileDef(name='merge_count_data.R',content=read_file('tools/merge_count_data.R'))
 MergeCounts<-Tool(id="MergeCounts",label="MergeCounts",
                     hints = requirements(docker(pull="bioconductor/devel_sequencing"),cpu(1),mem(2000)),
                     requirements=requirements(script),
-                    baseCommand = "RScript mergeCounts.R",
+                    baseCommand = "Rscript merge_count_data.R",
                     inputs = list(
                       input(
                         id= 'source_file',
-                        type='File Array',
+                        type=ItemArray("File"),
                         required=TRUE,
                         position=4),
                       input(
                         id='Column',
-                        type= 'numeric',
+                        type= 'int',
                         required=TRUE,
                         default="1",
                         position=1),
@@ -31,11 +31,11 @@ MergeCounts<-Tool(id="MergeCounts",label="MergeCounts",
                         type='string',
                         required=TRUE,
                         position=3,
-                        default='merged.csv',
+                        default='merged',
                         description='name for output file'
                       )
                     ),
-                    outputs=output(id = 'output',glob = "*merged.csv")
+                    outputs=output(id = 'output',glob = "*.csv")
 )
 
 p$app_add('mergecounts',MergeCounts)
--- tools/merge_count_data.R
+++ tools/merge_count_data.R
@@ -1,22 +1,14 @@
 # Merges count data from a list of inputs into a N-sample data table
-#` Usage: merge_count_data.R [Selected column of input file<numeric>] [comma-separated vector of sample names<string>] [samples <Files>]
+#` Usage: merge_count_data.R [Selected column of input file<numeric>] [comma-separated vector of sample names<string>] [output filename] [samples <Files>]
 
 arglist <- commandArgs(TRUE)
-
-require(data.table) # hope this is installed
-
-dlist <- lapply(x=arglist[4:length(arglist)],FUN = fread,select=as.integer(as.character(arglist[1]))) # make a scary huge list
-
-multmerge = function(x){
-  datalist = lapply(x[4:length(x)], FUN= function(x,select){data.table::fread(x,select=select)},select=as.integer(as.character(x[1])))
-  Reduce(function(x,y) {cbind(x,y)}, datalist)
-  }
-
-merdf <- multmerge(dlist)
+if(!require("data.table")){install.packages("data.table");require("data.table")}
+dlist <- lapply(X=arglist[4:length(arglist)],FUN = fread,select=as.integer(as.character(arglist[1]))) # make a scary huge list
+dlist <- Reduce(cbind,dlist)
 # Merged count list [assuming that outputs don't omit 0s, which i think is correct]
-colnames(merdf) <- unlist(strsplit(arglist[2],split=","))
+colnames(dlist) <- unlist(strsplit(arglist[2],split=","))
 # Set the names
 # Add in the isoform names
 inames<-fread(arglist[4],select=1)
-merdf<-cbind(inames,merdf)
-write.csv(merdf,file = arglist[3])
+dlist<-cbind(inames,dlist)
+write.csv(dlist,file = paste0(arglist[3],".csv"),quote = FALSE,row.names = FALSE)
